14b6c54d6800e8549afbf3826be2092defff182c
maryprecious <30366881+maryprecious@users.noreply.github.com>
2026-03-29 19:49:39 -0700

    Change splash screen delay from 1500ms to 3000ms

--- lib/features/onboarding/screens/splash-screen.dart
+++ lib/features/onboarding/screens/splash-screen.dart
@@ -41,7 +41,7 @@ class _SplashScreenState extends ConsumerState<SplashScreen>
     });
 
     // Navigate to Onboarding or Home
-    Future.delayed(const Duration(milliseconds: 1500), () async {
+    Future.delayed(const Duration(milliseconds: 3000), () async {
       if (mounted) {
         final authService = ref.read(authServiceProvider);
         final user = await authService.getCurrentUser();
